65f869ff1d19bddfbaa6196ee1d7a14271e39151
koljaPl <nikolya.plugin@gmail.com>
2026-02-22 22:06:34 +0100

    Added CORS

--- app/main.py
+++ app/main.py
@@ -1,6 +1,7 @@
 import uvicorn
 from fastapi import FastAPI, Depends, HTTPException
 from fastapi.security import OAuth2PasswordRequestForm, OAuth2PasswordBearer
+from fastapi.middleware.cors import CORSMiddleware
 
 from app.routers import posts
 from app.auth import verify_password, create_access_token, get_password_hash
@@ -9,6 +10,21 @@ from app.config import settings
 app = FastAPI(title=settings.PROJECT_NAME)
 
 app.include_router(posts.router)
+
+origins = [
+    "http://localhost:3000",    # Твой фронтенд (React/Vue)
+    "http://127.0.0.1:3000",
+    # "https://myblog.com",     # Твой будущий домен в продакшене
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,            # Кому разрешаем доступ
+    allow_credentials=True,
+    allow_methods=["*"],              # Разрешаем все методы (GET, POST, PUT, DELETE)
+    allow_headers=["*"],              # Разрешаем любые заголовки
+)
+
 
 @app.post("/login")
 async def login(form_data: OAuth2PasswordRequestForm = Depends()):
